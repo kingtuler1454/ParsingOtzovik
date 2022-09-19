e8c1c6f81bcabb72935210958382abcdf844d9fd
kingtuler1454 <morozovsergey.03@gmail.com>
2022-09-19 11:11:06 +0400

    write opinion

---
+++ opinion.py
@@ -0,0 +1,22 @@
+import requests
+import info
+import page_opinions
+from bs4 import BeautifulSoup
+
+def opinion(i,j, href):
+    html_text=requests.get('https://otzovik.com/'+href, headers={"User-Agent": info.user_agent}).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    plus=soup.find(class_='review-plus') # находим достоинство
+    minus=soup.find(class_='review-minus') # находим негатив
+    opinion=soup.find(class_='description') # находим отзыв
+    if len(str(j))>=3: # 1
+        namefile='0'+str(j)
+    elif len(str(j))>=2:
+        namefile='00'+str(j)
+    elif len(str(j))>=1:
+        namefile='000'+str(j)
+    else:
+        namefile="0000"+str(j)
+    text='Достоинства:\n'+plus.text+'\n'+'Недостатки:\n'+minus.text+'\n'+'Отзыв:\n'+ opinion.text
+    with open('dataset/'+str(i)+'/'+namefile+'.txt', 'w+') as file:
+        file.write(text) 
--- page_opinions.py
+++ page_opinions.py
@@ -1,12 +1,12 @@
 import requests
-import info.py
+import info
+import opinion
 from bs4 import BeautifulSoup
 
-def page_opinions(j,i):
-
+def page_opinions(j ,i):
     html_text=requests.get(info.base_URL+'/'+str(j)+'/?ratio='+str(i), headers={"User-Agent": info.user_agent}).text
-
     soup = BeautifulSoup(html_text, 'lxml')
-    page_opinion=soup.find('a',class_='review-btn review-read-link', href=True) # находим кнопку посследней макс страницы
-    print(page_opinion)
-    #review-btn review-read-link
+    page_opinion=soup.find_all('a',class_='review-btn review-read-link', href=True) # находим кнопку  ссылок
+    for element in page_opinion: # для каждого элемента будем сплитом делить и находить ссылку href
+        spisok=str(element).split('"') # spisok[3] это ссылка
+        opinion(i,j,spisok[3])
