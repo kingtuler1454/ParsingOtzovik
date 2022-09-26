01adc99c260a41023361767533108585fa26f673
kingtuler1454 <morozovsergey.03@gmail.com>
2022-09-26 07:39:07 +0400

    finish

--- info.py
+++ info.py
@@ -2,5 +2,5 @@ base_URL = "https://otzovik.com/reviews/sberbank_rossii"
 user_agent = 'Mozilla/5.0 '
 proxies={
     'http': 'http://10.10.1.10:3128',
-    'https': 'http://10.10.1.10:1080',
+'https': 'http://10.10.1.10:1080',
 }
