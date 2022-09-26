0d2d5b70763d12d6c86564d93b5a2e6c56ad0d12
kingtuler1454 <morozovsergey.03@gmail.com>
2022-09-26 09:19:29 +0400

    finicsh pep8 and get function and namefile

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import function_work_html_ratio
 import os
+
+import function_work_html_ratio
+
+
 def main():
-    for i in range(1,6):
-        os.makedirs("dataset/"+str(i)) # для каждой звезды
-        print('Ищем отзывы для звезды '+str(i))
+    for i in range(1, 6):
+        os.makedirs("dataset/" + str(i))  # для каждой звезды
+        print(f'Ищем отзывы для звезды {str(i)}')
         function_work_html_ratio.function_work_html_ratio(i)
 
 
